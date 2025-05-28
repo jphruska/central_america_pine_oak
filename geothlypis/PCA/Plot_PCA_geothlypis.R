fc49cd55e5de5c4d0a40313f39550cb5c74a4d91
Jack Hruska <jackphruska@gmail.com>
2025-05-28 17:30:03 -0500

    add Geothlypis PCA

---
+++ geothlypis/PCA/Plot_PCA_geothlypis.R
@@ -0,0 +1,53 @@
+## PLotting PCA Results from Plink ##
+
+library(tidyverse)
+library(stringr)
+library(colourpicker)
+library(RColorBrewer)
+
+# Set all the population vectors for each individual
+
+pop <- as.factor(c("Highland_El_Salvador", "Highland_Nicaragua", "Highland_Nicaragua", 
+                   "Lowland_Nicaragua", 
+                   "Highland_El_Salvador")) 
+
+
+
+# read in eigen vector and eigen value files 
+pca <- read_table2( "plink_out_structure_geothlypis.eigenvec", col_names = FALSE)
+eigenval <- scan("plink_out_structure_geothlypis.eigenval")
+
+# sort out the pca data
+
+# remove nuisance column (due to duplicate IDs introduced by plink)
+pca <- pca[,-1]
+
+# set names
+names(pca)[1] <- "ind"
+names(pca)[2:ncol(pca)] <- paste0("PC", 1:(ncol(pca)-1))
+pca$Population <- pop
+pca <- as_tibble(pca)
+
+# first convert to percentage variance explained
+pve <- data.frame(PC = 1:nrow(pca), pve = eigenval/sum(eigenval)*100)
+
+# make plot
+print(ggplot(pve, aes(PC, pve)) + geom_bar(stat = "identity")
+      + ylab("Percentage variance explained") + theme_light()) 
+
+# calculate the cumulative sum of the percentage variance explained
+cumsum(pve$pve)
+colour <- c("#00FFFF", "#990099", "#CCCC00") 
+
+a <- ggplot(pca, aes(PC1, PC2, fill = Population)) + 
+  geom_point(size = 10, alpha = 0.75, shape = 21, show.legend = FALSE) +
+  scale_fill_manual(values=colour) +
+  coord_equal() + theme_bw() +
+  xlab(paste0("PC1 (", signif(pve$pve[1], 3), "%)")) + 
+  theme(plot.margin = unit(c(0, 0, 0, 0), "cm"),) +
+  xlim(c(-0.9, 0.9)) +
+  ylim(c(-0.9, 0.9)) +
+  ylab(paste0("PC2 (", signif(pve$pve[3], 3), "%)"))
+
+    
+plot(a)
